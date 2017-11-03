d01001b92bed1f922ba1e5b3999de2d7e384d5bd
Mike Murphy <mike@brainstormchaser.com>
2017-11-03 13:31:28 -0700

    Updated seed file

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,3 +5,23 @@ INSERT INTO events (type) VALUES ('Wedding');
 INSERT INTO events (type) VALUES ('Holiday Party');
 INSERT INTO events (type) VALUES ('Housewarming Party');
 INSERT INTO events (type) VALUES ('Holiday Party');
+
+
+INSERT INTO alcohol (type,name,image) VALUES ("Spirits","Forty Creek Barrel Select Whisky
+","https://dx5vpyka4lqst.cloudfront.net/products/8805/images/thumb.png")
+INSERT INTO alcohol (type,name,image) VALUES ("Spirits","Crown Royal Whisky
+","https://dx5vpyka4lqst.cloudfront.net/products/114694/images/thumb.png")
+INSERT INTO alcohol (type,name,image) VALUES ("Beer","Coors Light
+","https://dx5vpyka4lqst.cloudfront.net/products/906560/images/thumb.png")
+INSERT INTO alcohol (type,name,image) VALUES ("Wine","Jackson-Triggs Pinot Grigio Bag in Box
+","https://dx5vpyka4lqst.cloudfront.net/products/270959/images/thumb.png")
+INSERT INTO alcohol (type,name,image) VALUES ("Beer","Labatt Blue","https://dx5vpyka4lqst.cloudfront.net/products/900480/images/thumb.png")
+INSERT INTO alcohol (type,name,image) VALUES ("Wine","Jackson-Triggs Cabernet Sauvignon Bag in Box
+","https://dx5vpyka4lqst.cloudfront.net/products/270884/images/thumb.png")
+INSERT INTO alcohol (type,name,image) VALUES ("Spirits","J.P. Wiser's Deluxe Whisky
+","https://dx5vpyka4lqst.cloudfront.net/products/893/images/thumb.png")
+INSERT INTO alcohol (type,name,image) VALUES ("Beer","Heineken","https://dx5vpyka4lqst.cloudfront.net/products/337949/images/thumb.png")
+INSERT INTO alcohol (type,name,image) VALUES ("Spirits","Smirnoff Vodka
+","https://dx5vpyka4lqst.cloudfront.net/products/38505/images/thumb.png")
+INSERT INTO alcohol (type,name,image) VALUES ("Wine","French Cross Dry White Bag in Box
+","https://dx5vpyka4lqst.cloudfront.net/products/589051/images/thumb.png")
