7a45d03ad5206e9fb8617d5ad31b2092b149fd5d
david morse <davidbryanmarshall@gmail.com>
2017-11-03 15:14:17 -0700

    changing table names back to uppercase

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,29 +1,29 @@
 USE alcohol_db;
 
-INSERT INTO events (type) VALUES ('Birthday Party');
-INSERT INTO events (type) VALUES ('Dinner Party');
-INSERT INTO events (type) VALUES ('Company Party');
-INSERT INTO events (type) VALUES ('Wedding');
-INSERT INTO events (type) VALUES ('Holiday Party');
-INSERT INTO events (type) VALUES ('Housewarming Party');
-INSERT INTO events (type) VALUES ('Holiday Party');
+INSERT INTO Events (type) VALUES ('Birthday Party');
+INSERT INTO Events (type) VALUES ('Dinner Party');
+INSERT INTO Events (type) VALUES ('Company Party');
+INSERT INTO Events (type) VALUES ('Wedding');
+INSERT INTO Events (type) VALUES ('Holiday Party');
+INSERT INTO Events (type) VALUES ('Housewarming Party');
+INSERT INTO Events (type) VALUES ('Holiday Party');
 
 
-INSERT INTO alcohol (type,name,image) VALUES ("Spirits","Forty Creek Barrel Select Whisky
+INSERT INTO Alcohol (type,name,image) VALUES ("Spirits","Forty Creek Barrel Select Whisky
 ","https://dx5vpyka4lqst.cloudfront.net/products/8805/images/thumb.png");
-INSERT INTO alcohol (type,name,image) VALUES ("Spirits","Crown Royal Whisky
+INSERT INTO Alcohol (type,name,image) VALUES ("Spirits","Crown Royal Whisky
 ","https://dx5vpyka4lqst.cloudfront.net/products/114694/images/thumb.png");
-INSERT INTO alcohol (type,name,image) VALUES ("Beer","Coors Light
+INSERT INTO Alcohol (type,name,image) VALUES ("Beer","Coors Light
 ","https://dx5vpyka4lqst.cloudfront.net/products/906560/images/thumb.png");
-INSERT INTO alcohol (type,name,image) VALUES ("Wine","Jackson-Triggs Pinot Grigio Bag in Box
+INSERT INTO Alcohol (type,name,image) VALUES ("Wine","Jackson-Triggs Pinot Grigio Bag in Box
 ","https://dx5vpyka4lqst.cloudfront.net/products/270959/images/thumb.png");
-INSERT INTO alcohol (type,name,image) VALUES ("Beer","Labatt Blue","https://dx5vpyka4lqst.cloudfront.net/products/900480/images/thumb.png");
-INSERT INTO alcohol (type,name,image) VALUES ("Wine","Jackson-Triggs Cabernet Sauvignon Bag in Box
+INSERT INTO Alcohol (type,name,image) VALUES ("Beer","Labatt Blue","https://dx5vpyka4lqst.cloudfront.net/products/900480/images/thumb.png");
+INSERT INTO Alcohol (type,name,image) VALUES ("Wine","Jackson-Triggs Cabernet Sauvignon Bag in Box
 ","https://dx5vpyka4lqst.cloudfront.net/products/270884/images/thumb.png");
-INSERT INTO alcohol (type,name,image) VALUES ("Spirits","J.P. Wiser's Deluxe Whisky
+INSERT INTO Alcohol (type,name,image) VALUES ("Spirits","J.P. Wiser's Deluxe Whisky
 ","https://dx5vpyka4lqst.cloudfront.net/products/893/images/thumb.png");
-INSERT INTO alcohol (type,name,image) VALUES ("Beer","Heineken","https://dx5vpyka4lqst.cloudfront.net/products/337949/images/thumb.png");
-INSERT INTO alcohol (type,name,image) VALUES ("Spirits","Smirnoff Vodka
+INSERT INTO Alcohol (type,name,image) VALUES ("Beer","Heineken","https://dx5vpyka4lqst.cloudfront.net/products/337949/images/thumb.png");
+INSERT INTO Alcohol (type,name,image) VALUES ("Spirits","Smirnoff Vodka
 ","https://dx5vpyka4lqst.cloudfront.net/products/38505/images/thumb.png");
-INSERT INTO alcohol (type,name,image) VALUES ("Wine","French Cross Dry White Bag in Box
+INSERT INTO Alcohol (type,name,image) VALUES ("Wine","French Cross Dry White Bag in Box
 ","https://dx5vpyka4lqst.cloudfront.net/products/589051/images/thumb.png");
