72d070cd12129d00fdc79f6ea773eeca1bf7684b
david morse <davidbryanmarshall@gmail.com>
2017-11-04 22:58:24 -0700

    reworked lcbo script to handle all inserts in one, and updated README

--- db/lcbo.js
+++ db/lcbo.js
@@ -1,16 +1,11 @@
-// var mysql = require("mysql");
 var request = require("request");
 var mysql = require("mysql2");
 
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
-
-  // Your username
   user: "root",
-
-  // Your password
-  password: "Snowboarder1$",
+  password: "root",
   database: "alcohol_db"
 });
 
@@ -19,37 +14,41 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId + "\n");
 });
 
-    function getLcboData() {
+function getLcboData() {
 
-    var qUrl = "https://lcboapi.com/products?per_page=100&access_key=MDpjOTBmZTUyMC1jMWNiLTExZTctYjg4MC02YmZmM2Y5NTY3NDE6VkxhMzdqNHcweFNhRkJrb3cyYXFpZVFxYW5JaklkUWZ4TDV5";
+  var qUrl = "https://lcboapi.com/products?per_page=100&access_key=MDpjOTBmZTUyMC1jMWNiLTExZTctYjg4MC02YmZmM2Y5NTY3NDE6VkxhMzdqNHcweFNhRkJrb3cyYXFpZVFxYW5JaklkUWZ4TDV5";
 
+  request.get(qUrl, function(error, response,body) {
 
-    request.get(qUrl, function(error, response,body){
+  if (error) throw error;
+  
+  var objBody = JSON.parse(body);
+  var data = objBody.result;
 
-        if (error) throw error;
-        var objBody = JSON.parse(body);
-        var data = objBody.result;
+  var arrayOfAlcohols = [];
+  
+  for(var i = 0; i < data.length; i++) {
+    arrayOfAlcohols.push(
+      [
+        data[i].primary_category,
+        data[i].name,
+        data[i].image_thumb_url,
+        data[i].tags
+      ]
+    );
+  };
 
-       var arrayOfAlcohols = [];
-        
-        for(var i = 0; i < data.length; i++) {
-         
-         var query = connection.query(
-          "INSERT INTO Alcohol SET ?",
-          {
-            type: data[i].primary_category,
-            name: data[i].name,
-            image: data[i].image_thumb_url,
-            tag: data[i].tags
-          },function(err,res) {
-            if (err) throw err;
-            else  {
-              console.log(res.affectedRows + " product inserted!\n");
-            };
-          })
-        }
-      });
+  var query = 'INSERT INTO Alcohol (type, name, image, tag) VALUES ?';
+  connection.query(query, [arrayOfAlcohols], 
+    function(err, res) {
+    if (err) throw err;
+      console.log(res.affectedRows + " products inserted!\n");
+      connection.end();
+  });
+
+  });
+
 }  
 
 getLcboData();
-    
+
