a16e2071167a86af506365150c961a9389f76bbf
Mike Murphy <mike@brainstormchaser.com>
2017-11-04 18:49:31 -0700

    sequalized event filter table with seeds

--- models/event-filter.js
+++ models/event-filter.js
@@ -1,7 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
   var EventFilter = sequelize.define("EventFilter", {
-    event Id : DataTypes.INTEGER,
-    alcohol Id : DataTypes.INTEGER,
+    eventId : DataTypes.INTEGER,
+    alcoholId : DataTypes.INTEGER,
      },  {
       timestamp : false,
       createdAt: false,
