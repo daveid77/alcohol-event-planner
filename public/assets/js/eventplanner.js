6fd742091e820162a32d8461fd9d932fd355b128
david morse <davidbryanmarshall@gmail.com>
2017-11-05 14:56:49 -0800

    added basic template for Event Alcohol Selection view with interface JS only

--- public/assets/js/eventplanner.js
+++ public/assets/js/eventplanner.js
@@ -1,40 +1,50 @@
 // Wait to attach handlers until DOM loaded
 $(function() {
-  $('.change-devoured').on('click', function(event) {
-    var id = $(this).data('id');
 
-    var newDevourState = {
-      devoured: true
-    };
-
-    // Send PUT request
-    $.ajax('/api/burgers/' + id, {
-      type: 'PUT',
-      data: newDevourState
-    }).then(
-      function() {
-        // Reload page for updated list
-        location.reload();
-      }
-    );
+  $('.alcohol-selection-wrapper').on('click', function(event) {
+    $(this).toggleClass('selected');
+    var text = $(this).attr('title') ;
+    $(this).attr('title', text);
   });
 
-  $('#alchlists').on('submit', function(event) {
-    event.preventDefault();
 
-    var newList = {
-      name: $('').val().trim(),
-    };
+  // two functions below may prove useful in this alcohol app
+  // $('.change-devoured').on('click', function(event) {
+  //   var id = $(this).data('id');
 
-    // Send POST request.
-    $.ajax('/api/burgers', {
-      type: 'POST',
-      data: newList
-    }).then(
-      function() {
-        // Reload page for updated list
-        location.reload();
-      }
-    );
-  });
+  //   var newDevourState = {
+  //     devoured: true
+  //   };
+
+  //   // Send PUT request
+  //   $.ajax('/api/burgers/' + id, {
+  //     type: 'PUT',
+  //     data: newDevourState
+  //   }).then(
+  //     function() {
+  //       // Reload page for updated list
+  //       location.reload();
+  //     }
+  //   );
+  // });
+
+  // $('#alchlists').on('submit', function(event) {
+  //   event.preventDefault();
+
+  //   var newList = {
+  //     name: $('').val().trim(),
+  //   };
+
+  //   // Send POST request.
+  //   $.ajax('/api/burgers', {
+  //     type: 'POST',
+  //     data: newList
+  //   }).then(
+  //     function() {
+  //       // Reload page for updated list
+  //       location.reload();
+  //     }
+  //   );
+  // });
+
 });
