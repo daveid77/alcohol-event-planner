603a30a86ce44303c62d2e194a09ef78f156e23a
Joshua Siverson <joshuasiverson@Joshuas-MacBook-Air.local>
2017-11-06 16:16:04 -0800

    updating keys

--- config/keys-BKUP.js
+++ config/keys-BKUP.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   google: {
-    clientID : "535357309167-dtp9lqqkvoovg7crf3kb802tv289jilp.apps.googleusercontent.com",
-    clientSecret : "j9WOlsBm7G43SLGPb10qH-WC"
+    clientID : "",
+    clientSecret : ""
   }
 }
