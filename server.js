79b267411e3afff532b8b1c8b5c3fbc99ef17a98
david morse <davidbryanmarshall@gmail.com>
2017-11-09 21:34:10 -0800

    moving route requires to after server invoked

--- server.js
+++ server.js
@@ -39,11 +39,7 @@ app.set("view engine", "handlebars");
 
 // Routes
 // =============================================================
-require("./routes/html-routes.js")(app);
-require("./routes/user-api-routes.js")(app);
-require("./routes/event-api-routes.js")(app);
-require("./routes/alcohol-api-routes.js")(app);
-require("./routes/occasion-api-routes.js")(app);
+
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
@@ -51,6 +47,11 @@ db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
     console.log("Database looks fine!");
+    require("./routes/html-routes.js")(app);
+    require("./routes/user-api-routes.js")(app);
+    require("./routes/event-api-routes.js")(app);
+    require("./routes/alcohol-api-routes.js")(app);
+    require("./routes/occasion-api-routes.js")(app);
     // require("./db/lcbo.js")(app);
   });
 });
