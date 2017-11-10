16015ee22ad47c2e92c47d55747de322577d96fe
david morse <davidbryanmarshall@gmail.com>
2017-11-09 23:21:53 -0800

    changing path for server lcbo call

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
     console.log("Database looks fine!");
-    require("./lcbo.js")(app);
+    require("lcbo.js")(app);
   });
 });
 
